1f0dc82a85b43693778a275f54aaaecac8a724ba
Zuzka Stavjaňová <zuzanastavjanova@gmail.com>
2022-10-24 22:47:29 +0200

    Add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 // https://astro.build/config
 import react from "@astrojs/react";
-import node from '@astrojs/node';
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
     integrations: [react()],
     output: "server",
-    adapter: node({
-        mode: 'standalone'
-    }),
+    adapter: vercel()
 });
