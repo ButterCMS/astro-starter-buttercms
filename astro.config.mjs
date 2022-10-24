c63fb62206a48d721b976c60a3eda318827d611c
Zuzka Stavjaňová <zuzanastavjanova@gmail.com>
2022-10-24 21:48:19 +0200

    Initial commit and readme setup

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// https://astro.build/config
+import react from "@astrojs/react";
+import node from '@astrojs/node';
+
+export default defineConfig({
+    integrations: [react()],
+    output: "server",
+    adapter: node({
+        mode: 'standalone'
+    }),
+});
